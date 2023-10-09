8a3a94695f1ec396d2a93d467d7b31df5c751dc8
row-brown <retbrown@gmail.com>
2023-10-09 11:53:40 -0400

    lab5part3 test6

--- catform.php
+++ catform.php
@@ -6,8 +6,8 @@
     <body>
         <p><?= var_dump($_GET) ?></p>
         <p><?= var_dump($_POST) ?></p>
-        <p>Your email is: <?= htmlspecialchars($_GET['signup'])?></p>
-        <p>You rated these cats <?= htmlspecialchars($_GET['rating'])?></p>
+        <p>Your email is: <?= htmlspecialchars($_POST['signup'])?></p>
+        <p>You rated these cats <?= htmlspecialchars($_POST['rating'])?></p>
         <p>If this rating is too low, we will contact you</p>
     </body>
 </html>
