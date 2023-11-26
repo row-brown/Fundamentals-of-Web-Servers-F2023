69dd52f296b43054243286882914a2c7c234bd72
row-brown <retbrown@gmail.com>
2023-11-26 10:04:59 -0500

    Lab 11 test 7

--- ledset.php
+++ ledset.php
@@ -5,19 +5,19 @@
         <script>
             function set_led(x) {
                 if(x==="ON"){
-                    $output = shell_exec("gpio write 0 1")
-                    echo "LED turned ON"
+                    $output = shell_exec("gpio write 0 1");
+                    echo "LED turned ON";
                 }
                 if(x==="OFF"){
-                    $output = shell_exec("gpio write 0 0")
-                    echo "LED turned OFF"
+                    $output = shell_exec("gpio write 0 0");
+                    echo "LED turned OFF";
                 }
             }
         </script>
     </head>
     <body>
-        <?= echo htmlspecialchars($_GET['ledset'])?>
-        <?= set_led($htmlspecialchars($_GET['ledset']))?>
+        <?= echo $set=htmlspecialchars($_GET['ledset']);?>
+        <?= set_led(htmlspecialchars($_GET['ledset']));?>
     </body>
 </html>
 
